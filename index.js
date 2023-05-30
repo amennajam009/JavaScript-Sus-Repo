7086686c8e896086c546467e9fcf7274a603b7a1
trench009 <amennajam7@gmail.com>
2023-05-31 00:41:14 +0500

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const submitButton = document.getElementById('submitButton');
 const outputElement = document.getElementById('output');
 const inputField2 = document.getElementById('inputField2');
 const submitButton2 = document.getElementById('submitButton2');
-
+const outputElement2 = document.getElementById('output1');
 
 const reverseString = (inputValue) => {
     return inputValue.split('').reverse().join('');
@@ -23,3 +23,19 @@ submitButton.addEventListener('click', function(event) {
  function remove(){
     this.outputElement.remove();
 }
+
+const MyTableFuntion = (GivenTable) =>{
+    let Result = ''
+    for(i=1;i<=10;i++){
+        let TableRow = GivenTable + 'X' + i + '=' + GivenTable * i;
+        Result += TableRow +'\n';
+    }
+    return Result;
+    
+}
+submitButton2.addEventListener('click' , function(event){
+ event.preventDefault();
+ const GivenTable = inputField2.value;
+ const Result = MyTableFuntion(GivenTable)
+ outputElement2.textContent = Result
+});
